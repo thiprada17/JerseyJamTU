cf2aa3d7f70ab0ae9e8bfd41f7683bb82f2e9166
thiprada17 <thiprada.mop@dome.tu.ac.th>
2025-09-06 00:01:02 +0700

    new font + folder

--- src/frontend/commu/commu.jsx
+++ src/frontend/commu/commu.jsx
@@ -1,11 +1,12 @@
 import { useRef, useEffect } from "react";
+import "./commu.css";
 
 
 export default function commu() {
 
     return (
         <div className="commu-body">
-                <div className="commu-navbar ">
+                <div className="commu-navbar w-100%">
                     JerseyJamTU
                 </div>
 
