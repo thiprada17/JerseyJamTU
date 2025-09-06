58dbe2f2ef9653861354f5d03b72ceac20d6e48e
FALONYN <lookfai1339@gmail.com>
2025-09-06 18:56:05 +0700

    signup without font na

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,6 @@
 import { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
-import SignIn from "./frontend/SignIn";
-import SignUp from "./frontend/SignUp";
-
-=======
-// import SignIn from "./frontend/SignIn";
-// import SignUp from "./frontend/SignUp";
->>>>>>> b31f245cf7458526b563ca6c15bcf1de942a305b
-// import Login from "./pages/Login";
-// import Register from "./pages/Register";
 import Commu from './frontend/commu/commu'
 import Homepage from './frontend/Homepage';
 
