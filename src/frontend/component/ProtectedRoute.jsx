cedfe8bdbd739c10d319ad84acf12f2bbd73aba9
thiprada17 <thiprada.mop@dome.tu.ac.th>
2025-11-13 16:15:45 +0700

    เพิ่ม loading ตรง login เทสๆ

--- src/frontend/component/ProtectedRoute.jsx
+++ src/frontend/component/ProtectedRoute.jsx
@@ -38,7 +38,7 @@ function ProtectedRoute({ children }) {
     verify();
   }, []);
 
-  if (verified === null) return <div>Loading...</div>;
+  if (verified === null) return <div className="token-loading">Loading...</div>;
   if (!verified) return <Navigate to="/" replace />;
 
   return children;
